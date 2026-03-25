19f28c3ebb466133c46165dba0e774f9bda291a6
Shashi <shashi070910@gmail.com>
2026-03-25 19:15:31 +0530

    Added Tasks DTOs

---
+++ src/WorkManagement.Application/Features/Tasks/DTOs/CreateTaskRequestDto.cs
@@ -0,0 +1,7 @@
+﻿public class CreateTaskRequestDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Guid ProjectId { get; set; }
+    public Guid AssignedUserId { get; set; }
+}
---
+++ src/WorkManagement.Application/Features/Tasks/DTOs/TaskResponseDto.cs
@@ -0,0 +1,6 @@
+﻿public class TaskResponseDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Status { get; set; }
+}
